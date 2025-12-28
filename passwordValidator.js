c5fdb8de024ac8fe12e24a67d606aff44d69767f
Ines <iliu@bordeaux-inp.fr>
2025-12-28 23:14:02 +0100

    TDD password length

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,7 +1,4 @@
 function validatePassword(password) {
-    if (password.length < 8) return false;
-    if (!/\d/.test(password)) return false;
-    if (!/[a-zA-Z]/.test(password)) return false;
-    return true;
+    return password.length >= 8;
 }
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -2,18 +2,6 @@
 const validatePassword = require('./passwordValidator');
 
 test('password length', () => {
-    expect(validatePassword('123non')).toBe(false);
+    expect(validatePassword('1234non')).toBe(false);
     expect(validatePassword('12345oui')).toBe(true);
-})
-
-test('number in password', () => {
-    expect(validatePassword('nooooooo')).toBe(false);
-    expect(validatePassword('ouiiiii1')).toBe(true);
-})
-
-test('letter in password', () => {
-    expect(validatePassword('12345678')).toBe(false);
-    expect(validatePassword('1234567&')).toBe(false);
-    expect(validatePassword('1234567y')).toBe(true);
-    expect(validatePassword('1234567Y')).toBe(true);
 })
