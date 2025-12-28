75742352e572c535c8802bed3d2d7915edb2c801
Ines <iliu@bordeaux-inp.fr>
2025-12-28 23:01:35 +0100

    at least 1 letter

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,6 +1,7 @@
 function validatePassword(password) {
     if (password.length < 8) return false;
     if (!/\d/.test(password)) return false;
+    if (!/[a-zA-Z]/.test(password)) return false;
     return true;
 }
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -10,3 +10,10 @@ test('number in password', () => {
     expect(validatePassword('nooooooo')).toBe(false);
     expect(validatePassword('ouiiiii1')).toBe(true);
 })
+
+test('letter in password', () => {
+    expect(validatePassword('12345678')).toBe(false);
+    expect(validatePassword('1234567&')).toBe(false);
+    expect(validatePassword('1234567y')).toBe(true);
+    expect(validatePassword('1234567Y')).toBe(true);
+})
