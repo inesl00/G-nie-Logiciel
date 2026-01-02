135fe35bc9bbaaae3ca425726493785d7a4ecd53
Ines <iliu@bordeaux-inp.fr>
2026-01-02 15:56:18 +0100

    P2 custom number and letter

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,9 +1,11 @@
 function validatePassword(password, options) {
     const minLength = options?.minLength ?? 8;
+    const minNumber = options?.minNumber ?? 1;
+    const minLetter = options?.minLetter ?? 1;
     
     if (password.length < minLength) return false;
-    if (!/\d/.test(password)) return false;
-    if (!/[a-zA-Z]/.test(password)) return false;
+    if ((password.match(/\d/g) || []).length < minNumber) return false;
+    if ((password.match(/[a-zA-Z]/g) || []).length < minLetter) return false;
     return true;
 }
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -20,3 +20,13 @@ test('password custom length', () => {
     expect(validatePassword('12345678a', {minLength: 10})).toBe(false);
     expect(validatePassword('123456789a', {minLength: 10})).toBe(true);
 })
+
+test('custom number in password', () => {
+    expect(validatePassword('noooooo1', {minNumber : 2})).toBe(false);
+    expect(validatePassword('ouiiii12', {minNumber : 2})).toBe(true);
+})
+
+test('custom letter in password', () => {
+    expect(validatePassword('1234567n', {minLetter : 2})).toBe(false);
+    expect(validatePassword('123456ou', {minLetter : 2})).toBe(true);
+})
