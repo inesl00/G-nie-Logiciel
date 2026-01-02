a925c0f73f6dea8ee413104c9d611cc1d0483c19
Ines <iliu@bordeaux-inp.fr>
2026-01-02 21:09:54 +0100

    Exercice 2: balance returned from DAO

--- bank.spec.js
+++ bank.spec.js
@@ -19,3 +19,13 @@ test('getBalance call retrieveBalance with accountId', () => {
     expect(spy).toHaveBeenCalledWith(accountId);
     spy.mockRestore();
 });
+
+test('getBalance return the balance retrieved from DAO', () => {
+    const spy = jest.spyOn(bankDAO, 'retrieveBalance').mockReturnValue(15);
+    const accountId = "12345";
+
+    const result = bank.getBalance(accountId);
+
+    expect(result).toBe(15);
+    spy.mockRestore();
+});
